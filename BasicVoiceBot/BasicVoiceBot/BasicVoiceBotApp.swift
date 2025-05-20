e08df42d6d41cb50b4afe8b08452b490b9d95556
janak2 <janakagrawal1309@gmail.com>
2025-05-19 21:32:34 -0700

    init code

---
+++ BasicVoiceBot/BasicVoiceBot/BasicVoiceBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicVoiceBotApp.swift
+//  BasicVoiceBot
+//
+//  Created by Janak Agrawal on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct BasicVoiceBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
